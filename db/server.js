661cba7acbd2f88a585605cfa4ff67ceed7644a2
Stanislaw Olszewski <s.olszewski.408@studms.ug.edu.pl>
2024-01-18 23:44:41 +0100

    fix: update cors values

--- db/server.js
+++ db/server.js
@@ -1,61 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const user = require('./models/user')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const {Server} = require('socket.io')
+const userRouter = require('./routes/user')
+const authRouter = require('./routes/auth')
 
 const app = express()
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
+app.use('/user', userRouter)
+app.use('/auth', authRouter)
 
 mongoose.connect('mongodb://localhost:27017')
 mongoose.connection.on('connected', () => console.log('connected to db'))
-
-//get all users
-app.get('/', async (req, res) => {
-    try {
-        const usr = await user.find()
-        res.status(200).json(usr)
-    } catch (error) {
-        res.status(500).json(error.message)
-    }
-})
-
-//create new user
-app.post('/create', async (req, res) => {
-    try {
-        const usr = new user({name: req.body.name, password: req.body.password, role: req.body.role})
-        await usr.save()
-        res.status(200).json({message: 'User created!'})
-    } catch (error) {
-        res.status(500).json(error.message)
-    }
-})
-
-//update existing user
-app.put('/update/:name', async (req, res) => {
-    try {
-        const users = await user.find({name: req.params.name})
-        if (users.length === 0) throw new Error('User not found!')
-        const existingUser = users[0]
-        existingUser.name = req.body.name
-        existingUser.password = req.body.password
-        existingUser.role = req.body.role
-        await existingUser.save()
-        res.status(200).json({message: 'User updated!'})
-    } catch (error) {
-        res.status(500).json(error.message)
-    }
-})
-
-//delete user
-app.delete('/delete/:name', async (req, res) => {
-    try {
-        const response = await user.deleteOne({name: req.params.name})
-        res.status(200).json({message: 'User deleted!'})
-    } catch (error) {
-        res.status(500).json(error.message)
-    }
-})
 
 const expressServer = app.listen(3000, () => {
     console.log('server listening on port 3000')
@@ -63,7 +26,7 @@ const expressServer = app.listen(3000, () => {
 
 const io = new Server(expressServer, {
     cors: {
-        origin: '*'
+        origin: 'http://localhost:5173'
     }
 })
 
