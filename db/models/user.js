31fd9b26d6b7933bd2ef7b6f30c04d1725d31bc2
Stanislaw Olszewski <s.olszewski.408@studms.ug.edu.pl>
2024-01-18 17:55:14 +0100

    feat: add password hashing and login

--- db/models/user.js
+++ db/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -15,6 +16,27 @@ const userSchema = new mongoose.Schema({
         enum: ['parent', 'child']
     }
 })
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+});
+
+userSchema.statics.login = async function(name, password) {
+    const user = await this.findOne({name})
+    if (!user) {
+        throw new Error('Incorrect name!')
+    }
+
+    const auth = await bcrypt.compare(password, user.password)
+    if (auth) {
+        return user
+    }
+
+    throw new Error('Incorrect password!')
+}
 
 const user = mongoose.model('user', userSchema)
 
