566c80e112853aadbb33547633d26cc155c82e7b
Stanislaw Olszewski <s.olszewski.408@studms.ug.edu.pl>
2024-01-18 23:37:39 +0100

    feat: forms send requests and get user data and cookie

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,18 +1,39 @@
 import { useFormik } from 'formik';
+import { useUser } from '../context/UserProvider';
+
 
 export default function LoginForm() {
+    const {dispatch, state} = useUser()
+    console.log(state)
     const formik = useFormik({
         initialValues: {
             name: '',
             password: '',
         },
         onSubmit: async (values, actions) => {
-            alert(JSON.stringify(values, null, 2));
-            const response = await fetch('http://localhost:3000/auth/login', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(values),
-            });
+            // alert(JSON.stringify(values, null, 2));
+            try {
+                const response = await fetch('http://localhost:3000/auth/login', {
+                    method: 'POST',
+                    credentials: 'include',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(values),
+                });
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const response2 = await fetch('http://localhost:3000/auth/', {
+                    credentials: 'include',
+                    headers: { 'Content-Type': 'application/json' }
+                });
+                if (!response2.ok) {
+                    throw new Error('Network response2 was not ok');
+                }
+                const data = await response2.json();
+                dispatch({type: 'LOGIN', payload: data})
+            } catch (error) {
+                console.log(error);
+            }
         },
     });
     return (
--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -1,38 +1,73 @@
-import {useFormik} from 'formik'
+import { useFormik } from 'formik';
+import { useUser } from '../context/UserProvider';
 
 export default function RegisterForm() {
+    const { dispatch } = useUser();
     const formik = useFormik({
         initialValues: {
             name: '',
             password: '',
-            role: 'user'
+            role: 'user',
         },
         onSubmit: async (values, actions) => {
-            alert(JSON.stringify(values, null, 2));
-            const response = await fetch('http://localhost:3000/auth/register', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(values),
-            });
+            try {
+                const response = await fetch(
+                    'http://localhost:3000/auth/register',
+                    {
+                        method: 'POST',
+                        credentials: 'include',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(values),
+                    }
+                );
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const response2 = await fetch('http://localhost:3000/auth/', {
+                    credentials: 'include',
+                    headers: { 'Content-Type': 'application/json' },
+                });
+                if (!response2.ok) {
+                    throw new Error('Network response2 was not ok');
+                }
+                const data = await response2.json();
+                dispatch({ type: 'LOGIN', payload: data });
+            } catch (error) {
+                console.log(error);
+            }
         },
-    
-    })
-    return <form>
-        <div>
-            <label htmlFor="name">Name</label>
-            <input id="name" type="text" {...formik.getFieldProps('name')}/>
-        </div>
-        <div>
-            <label htmlFor="password">Password</label>
-            <input id="password" type="password" {...formik.getFieldProps('password')}/>
-        </div>
-        <div>
-            <label htmlFor="role">Role</label>
-            <select name="role" id="role" value={formik.values.role} onChange={formik.handleChange}>
-                <option value="admin">Admin</option>
-                <option value="user">User</option>
-            </select>
-        </div>
-        <button type="submit">Register</button>
-    </form>
+    });
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input
+                    id="name"
+                    type="text"
+                    {...formik.getFieldProps('name')}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    type="password"
+                    {...formik.getFieldProps('password')}
+                />
+            </div>
+            <div>
+                <label htmlFor="role">Role</label>
+                <select
+                    name="role"
+                    id="role"
+                    value={formik.values.role}
+                    onChange={formik.handleChange}
+                >
+                    <option value="admin">Admin</option>
+                    <option value="user">User</option>
+                </select>
+            </div>
+            <button type="submit">Register</button>
+        </form>
+    );
 }
