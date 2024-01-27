7d0591e2a10f223892d5df104bdf71db30625eed
Stanislaw Olszewski <s.olszewski.408@studms.ug.edu.pl>
2024-01-27 19:35:16 +0100

    feat: add search posts with pattern matching

--- client/src/pages/Posts.jsx
+++ client/src/pages/Posts.jsx
@@ -4,30 +4,61 @@ import NewPostForm from '../components/NewPostForm';
 
 export default function PostsPage() {
     const [fetchHelper, setFetchHelper] = useState(false);
+    const [params, setParams] = useState({ key: 'title', value: '' });
     const [posts, setPosts] = useState([]);
     useEffect(() => {
         async function getPosts() {
+            let url = 'http://localhost:3000/posts'
+            if (params.value !== '') {
+                url += `?key=${params.key}&value=${params.value}`
+            }
+            console.log(url)
             try {
-                const response = await fetch('http://localhost:3000/posts/', {
+                const response = await fetch(url, {
                     credentials: 'include',
                 });
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
                 const data = await response.json();
-                console.log(data);
                 setPosts(data);
             } catch (error) {
                 console.log(error);
             }
         }
         getPosts();
-    }, [fetchHelper])
+    }, [fetchHelper, params]);
+    console.log(params)
     return (
         <div>
             <h1>Posts</h1>
-            <NewPostForm setHelper={setFetchHelper}/>
-            <DisplayPosts posts={posts} setHelper={setFetchHelper}/>
+            <div>
+                <label htmlFor="serach">Search</label>
+                <input
+                    type="text"
+                    value={params.value}
+                    onChange={(e) =>
+                        setParams((prev) => ({
+                            ...prev,
+                            value: e.target.value,
+                        }))
+                    }
+                />
+                <select
+                    name=""
+                    id=""
+                    onChange={(e) =>
+                        setParams((prev) => ({ ...prev, key: e.target.value }))
+                    }
+                >
+                    <option value="title">Title</option>
+                    <option value="content">Content</option>
+                    <option value="author">Author</option>
+                </select>
+                <button onClick={() => setFetchHelper(prev => !prev)}>Search</button>
+            </div>
+            <NewPostForm setHelper={setFetchHelper} />
+            <DisplayPosts posts={posts} setHelper={setFetchHelper} />
         </div>
     );
 }
--- db/routes/posts.js
+++ db/routes/posts.js
@@ -1,26 +1,42 @@
-const {Router} = require('express')
-const Post = require('../models/post')
-const Comment = require('../models/comment')
-const {checkAuthMiddleware} = require('../utils/auth')
+const { Router } = require('express');
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+const User = require('../models/user');
+const { checkAuthMiddleware } = require('../utils/auth');
 
-const router = Router()
-router.use(checkAuthMiddleware)
+const router = Router();
+router.use(checkAuthMiddleware);
 
 router.get('/', async (req, res) => {
     try {
-        const posts = await Post.find().populate('author', 'name').populate({
-            path: 'comments',
-            populate: {
-                path: 'author',
-                select: 'name',
-            }
-        })
-        res.status(200).json(posts)
+        let findObject = {};
+        if (req.query.key === 'author') {
+            const author = await User.findOne({
+                name: { $regex: req.query.value, $options: 'i' },
+            });
+            findObject = {
+                "author": author ? author._id : null,
+            };
+        } else if (req.query.value) {
+            findObject = {
+                [req.query.key]: { $regex: req.query.value, $options: 'i' },
+            };
+        }
+        const posts = await Post.find(findObject)
+            .populate('author', 'name')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'author',
+                    select: 'name',
+                },
+            })
+        res.status(200).json(posts);
     } catch (error) {
-        console.log(error)
-        res.status(500).json(error.message)
+        console.log(error);
+        res.status(500).json(error.message);
     }
-})
+});
 
 router.post('/', async (req, res) => {
     try {
@@ -29,39 +45,44 @@ router.post('/', async (req, res) => {
             content: req.body.content,
             author: res.locals.token.id,
             comments: [],
-        })
-        await post.save()
-        res.status(200).json({message: 'Post created!'})
+        });
+        await post.save();
+        res.status(200).json({ message: 'Post created!' });
     } catch (error) {
-        console.log(error)
-        res.status(500).json(error.message)        
+        console.log(error);
+        res.status(500).json(error.message);
     }
-})
+});
 
 router.put('/:id', async (req, res) => {
     try {
-        const post = await Post.findById(req.params.id)
-        if (post.author.toString() !== res.locals.token.id) throw new Error('You are not the author of this post!')
-        post.title = req.body.title
-        post.content = req.body.content
-        await post.save()
-        res.status(200).json({message: 'Post updated!'})
+        const post = await Post.findById(req.params.id);
+        if (post.author.toString() !== res.locals.token.id)
+            throw new Error('You are not the author of this post!');
+        post.title = req.body.title;
+        post.content = req.body.content;
+        await post.save();
+        res.status(200).json({ message: 'Post updated!' });
     } catch (error) {
-        console.log(error)
-        res.status(500).json(error.message)
+        console.log(error);
+        res.status(500).json(error.message);
     }
-})
+});
 
 router.delete('/:id', async (req, res) => {
     try {
-        const post = await Post.findById(req.params.id)
-        const comment = await Comment.deleteMany({_id: {$in: post.comments}})
-        const response = await Post.deleteOne({_id: req.params.id, author: res.locals.token.id})
-        res.status(200).json({message: 'Post deleted!'})
+        const post = await Post.findById(req.params.id);
+        const comment = await Comment.deleteMany({
+            _id: { $in: post.comments },
+        });
+        const response = await Post.deleteOne({
+            _id: req.params.id,
+            author: res.locals.token.id,
+        });
+        res.status(200).json({ message: 'Post deleted!' });
     } catch (error) {
-        res.status(500).json(error.message)
+        res.status(500).json(error.message);
     }
-})
+});
 
-
-module.exports = router
+module.exports = router;
